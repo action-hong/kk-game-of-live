34d402d4349cde2dfa0132a3a3992e9d57e21191
action-hong <kkopitehong@gmail.com>
2017-09-08 22:06:04 +0800

    完成简单的细胞生命游戏, 可自定义生成, 单步调试, 自动运行/停止等功能

---
+++ cell.js
@@ -0,0 +1,119 @@
+const random = () => Math.random() > 0.5 ? 1 : 0
+const style = ['gray', 'red']
+class Cell {
+  constructor(ctx, state) {
+    this.state = state
+    this.preState = null
+  }
+
+  changeState(state) {
+    if (state != 1 || state != 0) {
+      throw new Exception()
+    }
+    // 记录上一个状态, 优化绘制, 相同的话就不需要绘制了
+    this.preState = this.state
+    this.state = state
+  }
+
+  drawCell(row, col, w, h) {
+    // 优化绘制
+    if (this.state === this.preState) {
+      return
+    }
+    this.ctx.fillStyle = style[this.state]
+    this.ctx.fillRect(i*w, j*h, w - 1, h - 1)
+  }
+}
+
+class CellGonup {
+  constructor(row, col, ctx, w, h) {
+    this.row = row
+    this.col = col
+    this.cells= []
+    for (let i = 0; i < row ; i++) {
+      let cs = []
+      for (let j = 0 ; j < col ; j++) {
+        cs.push(random())
+      }
+      this.cells.push(cs)
+    }
+
+    this.ctx = ctx
+    this.canvasW = w
+    this.canvasH = h
+    this.w = w / col
+    this.h = h / row
+    this.style = ['gray', 'red']
+  }
+
+  nextRound() {
+    let nextCells = []
+    for (let i = 0; i < this.row ; i++) {
+      let cs = []
+      for (let j = 0 ; j < this.col ; j++) {
+        cs.push(this.nextState(i, j))
+      }
+      nextCells.push(cs)
+    }
+    this.cells = nextCells
+  }
+
+  /**
+   * 第i行j列 下一个状态是啥
+   * 活细胞(1), 周围有2个或3个活细胞则存活, 否则死掉
+   * 死细胞(0), 周围如果有 3个细胞, 则活起来, 变为1
+   * @param {*} i 
+   * @param {*} j 
+   */
+  nextState(i, j) {
+    let count = this.aroundLiveCells(i, j)
+    // console.log(i, j, count)
+    let state = this.cells[i][j]
+    if (state) {
+      if (count == 2 || count == 3) {
+        return 1
+      } else {
+        return 0
+      }
+    } else if (count === 3) {
+      return 1
+    }
+    return 0
+  }
+
+  aroundLiveCells(i, j) {
+    //(i-1, j-1), (i-1, j), (i-1, j+1), 
+    let count = 0
+    let pr = this.cells[i - 1]
+    let nr = this.cells[i + 1]
+    let cr = this.cells[i]
+    if (pr) {
+      pr[j-1] && count++
+      pr[j] && count++
+      pr[j+1] && count++
+    }
+
+    if (nr) {
+      nr[j-1] && count++
+      nr[j] && count++
+      nr[j+1] && count++
+    }
+
+    cr[j-1] && count++
+    cr[j+1] && count++
+
+    return count
+  }
+
+  draw() {
+    ctx.clearRect(0,0,this.canvasW, this.canvasH)
+    let w = this.w
+    let h = this.h
+    for (let i = 0; i < this.row ; i++) {
+      for (let j = 0 ; j < this.col ; j++) {
+        this.ctx.fillStyle = this.style[this.cells[i][j]]
+        this.ctx.fillRect(i*w, j*h, w - 1, h - 1)
+      }
+    }
+  }
+}
